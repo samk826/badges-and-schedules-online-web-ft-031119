0677e75515196ca9b3f25b0afc69b12327d34f70
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-16 18:40:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,13 @@ def batch_badge_creator(attendees)
     badge_maker(name)
   end
 
+  def assign_rooms(attendees)
+    room=0
+    attendees.collect do |name|
+      room+=1
+      "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+
 end
 
 def printer(attendees)
